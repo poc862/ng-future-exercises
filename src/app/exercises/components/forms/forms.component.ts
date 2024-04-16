31f4430436cef43a2774b50ddce188ccc646ee19
poc862 <109282744+poc862@users.noreply.github.com>
2024-04-16 08:16:23 +0000

    Day6

--- src/app/exercises/components/forms/forms.component.ts
+++ src/app/exercises/components/forms/forms.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core'
-import { ReactiveFormsModule } from '@angular/forms'
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import { DateTime } from 'luxon'
+
+interface HabitFormModel {
+  name: FormControl<string>
+  date: FormControl<DateTime>
+  completed?: FormControl<boolean>
+}
 
 @Component({
   selector: 'app-forms',
@@ -8,4 +15,16 @@ import { ReactiveFormsModule } from '@angular/forms'
   styleUrl: './forms.component.css',
   imports: [ReactiveFormsModule],
 })
-export class FormsComponent {}
+export class FormsComponent {
+  habits: { name: string | null | undefined; date: DateTime }[] = []
+  habitForm = new FormGroup({
+    habitName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+  })
+  onSubmit() {
+    if (this.habitForm.invalid) {
+      alert('Error')
+    } else {
+      this.habits.push({ name: this.habitForm.value.habitName, date: DateTime.now() })
+    }
+  }
+}
