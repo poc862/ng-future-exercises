57017d4fae14c7a665f326ff0c604ed88d7f9e2f
poc862 <109282744+poc862@users.noreply.github.com>
2024-04-16 10:45:59 +0000

    Day7

--- src/app/exercises/components/http-client/http-client.component.ts
+++ src/app/exercises/components/http-client/http-client.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core'
+import { lastValueFrom } from 'rxjs'
+import { HttpClient } from '@angular/common/http'
+
+type PostType = Omit<Post, 'id'>
+
+interface Post {
+  id: number
+  title: string
+  body: string
+  userId: number
+}
 
 @Component({
   selector: 'app-http-client',
   standalone: true,
-  template: ``,
+  styleUrl: './http-client.component.css',
+  templateUrl: './http-client.component.html',
 })
-export class HttpClientComponent {}
+export class HttpClientComponent {
+  posts: PostType[] = []
+
+  constructor(private readonly httpClient: HttpClient) {}
+
+  async getData() {
+    this.posts = await lastValueFrom(this.httpClient.get<PostType[]>('posts'))
+  }
+}
