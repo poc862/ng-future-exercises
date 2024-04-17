f3a1014bd08f34655c96b50d2ddc75a7ebd6f481
poc862 <109282744+poc862@users.noreply.github.com>
2024-04-17 08:11:58 +0000

    Day8

--- src/app/exercises/components/content-projection/exercise/content-projection.component.ts
+++ src/app/exercises/components/content-projection/exercise/content-projection.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
+import { ComponentWithNgContentComponent } from './component-with-ng-content/component-with-ng-content.component'
 
 @Component({
   selector: 'app-content-projection',
   templateUrl: './content-projection.component.html',
   styleUrl: './content-projection.component.css',
   standalone: true,
-  imports: [],
+  imports: [ComponentWithNgContentComponent],
 })
 export class ContentProjectionComponent {}
